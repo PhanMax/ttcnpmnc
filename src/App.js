0f249a4185ef8de91452123d283a6cf11a31cee7
DELL <21AK22@DELL.DELL>
2020-06-27 09:52:53 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
-import React from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Login from "./components/login.component";
 import SignUp from "./components/signup.component";
+import LadingPage from "./components/ladingpage.component";
+import LandingPage from "./components/ladingpage.component";
 
 function App() {
-  return (<Router>
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-        <div className="container">
-          <Link className="navbar-brand" to={"/sign-in"}>ABC</Link>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      <div className="auth-wrapper">
-        <div className="auth-inner">
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route path="/sign-in" component={Login} />
-            <Route path="/sign-up" component={SignUp} />
-          </Switch>
-        </div>
-      </div>
-    </div></Router>
+  return (
+    <Router>
+      <Route path = "/" exact component = {LandingPage} />
+      <Route path="/sign-in" component={Login} />
+      <Route path="/sign-up" component={SignUp} />
+    </Router>
   );
 }
 
 export default App;
+
+{
+  /* <div className="auth-wrapper">
+  <div className="auth-inner">
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route path="/sign-in" component={Login} />
+      <Route path="/sign-up" component={SignUp} />
+    </Switch>
+  </div>
+</div> */
+}
