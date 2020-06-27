74d3fc7babf40a8fe4e444eac8245728d0671496
Max Phan <max@vinova.sg>
2020-06-27 12:05:29 +0700

    Nhut commit - list posts page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+import Listitem from "./components/listitem.component";
 import Login from "./components/login.component";
 import SignUp from "./components/signup.component";
 
 function App() {
+  
   return (<Router>
+    
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container">
@@ -20,17 +23,24 @@ function App() {
               <li className="nav-item">
                 <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
               </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/listitem"}>list item</Link>
+              </li>
             </ul>
           </div>
         </div>
       </nav>
-
+      <div className="list-item-wrapper">
+        <Route path="/listitem" component={Listitem} />
+      </div>
       <div className="auth-wrapper">
         <div className="auth-inner">
           <Switch>
             <Route exact path='/' component={Login} />
             <Route path="/sign-in" component={Login} />
             <Route path="/sign-up" component={SignUp} />
+            
+            
           </Switch>
         </div>
       </div>
--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -1,32 +1,43 @@
 import React, { Component } from "react";
 
 export default class SignUp extends Component {
+    constructor(props) {
+      super(props);
+      // create a ref to store the textInput DOM element
+      this.usernameInput = React.createRef();
+      this.pwInput = React.createRef();
+      this.phoneInput = React.createRef();
+    }
+    onSubmit(e){
+        e.preventDefault();
+        console.log('The link was clicked.');
+        let username = this.usernameInput.current.value;
+        let pw = this.pwInput.current.value;
+        let phoneInput = this.phoneInput.current.value;
+        console.log(username, pw, phoneInput);
+
+    }
     render() {
         return (
             <form>
                 <h3>Sign Up</h3>
 
                 <div className="form-group">
-                    <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
+                    <label>Username: </label>
+                    <input ref = {this.usernameInput} type="text" className="form-control" placeholder="First name" />
                 </div>
 
                 <div className="form-group">
-                    <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
+                    <label>Password: </label>
+                    <input ref = {this.pwInput} type="password" className="form-control" placeholder="Enter password" />
                 </div>
 
                 <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <label>Phone: </label>
+                    <input ref = {this.phoneInput} type="text" className="form-control" placeholder="Last name" />
                 </div>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <button type="submit" className="btn btn-primary btn-block" onClick = {this.onSubmit}>Sign Up</button>
                 <p className="forgot-password text-right">
                     Already registered <a href="/sign-in">sign in?</a>
                 </p>
